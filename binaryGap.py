8ecca284750ae11f9f24eea9744e12d6f6860a24
Amman Das <70426448+AMMAND3@users.noreply.github.com>
2023-01-04 15:00:41 -0700

    Create binaryGap.py

---
+++ binaryGap.py
@@ -0,0 +1,18 @@
+def solution(N):
+    # Implement your solution here
+a =N val = ""
+    while (a >= 1):
+        b = a%2
+        val += str(b)
+        a = a//2
+    val = ''.join(reversed(val))
+    ls = 0
+st = ""
+    for x in val:
+        if x == "1":
+            if len(st) > 0 and "0" in st:
+                ls = max(ls, st.count("0"))
+                st = ""
+        elif x == "0":
+            st += "0"
+    return ls
