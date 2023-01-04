c224d6ddc97148767a954e55d47cbb389885c948
Amman Das <70426448+AMMAND3@users.noreply.github.com>
2023-01-04 15:04:29 -0700

    Update binaryGap.py

--- binaryGap.py
+++ binaryGap.py
@@ -1,13 +1,13 @@
 def solution(N):
     # Implement your solution here
-a =N val = ""
+    a =N val = ""
     while (a >= 1):
         b = a%2
         val += str(b)
         a = a//2
     val = ''.join(reversed(val))
     ls = 0
-st = ""
+    st = ""
     for x in val:
         if x == "1":
             if len(st) > 0 and "0" in st:
