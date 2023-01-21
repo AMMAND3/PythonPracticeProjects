f71db808940513d8405f8e0f65988d2a2ee79adc
Amman Das <70426448+AMMAND3@users.noreply.github.com>
2023-01-20 18:58:19 -0700

    Update flippingMatrices.py

--- flippingMatrices.py
+++ flippingMatrices.py
@@ -1,5 +1,14 @@
 def flippingMatrix(matrix):
-    # Write your code here
+    # remember
+    #     [0] [1] [2] [3]
+    # [0] a    b   b   a        
+    # [1] c    d   d   c    
+    # [2] c    d   d   c     
+    # [3] a    b   b   a
+    #
+    # so we only have to look at the indexes where it can be flippd as a quadrant,
+    # otherwise we would rather look at the sum of all quadrants
+    
     n = int(len(matrix)/2)
     maxv = 0
     total = 0
